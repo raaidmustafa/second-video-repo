8a737f9314cb697d94cf087fe8c13993ae5a2521
raaidmustafa <raaidmustafa159@gmail.com>
2025-07-15 11:49:20 +0500

    js

---
+++ script.js
@@ -0,0 +1,10 @@
+console.log("hello world")
+//this is a code of no use 
+/*this code prints 'hello world' on the google chrome console
+other than that the code has no use 
+this is a multiple line comment being used to elaborate the use for the code above*/
+//logical operators
+let a = 6;
+let b = 5;
+
+console.log("cond1 && cond2 = ", a < b && a === 6);
