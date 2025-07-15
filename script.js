b4e2e37edf91f9da04c507b957028b7ffc93b122
raaidmustafa <raaidmustafa159@gmail.com>
2025-07-15 12:19:46 +0500

    js

--- script.js
+++ script.js
@@ -8,3 +8,17 @@ let a = 6;
 let b = 5;
 
 console.log("cond1 && cond2 = ", a < b && a === 6);
+
+//unary operators
+let c = 5;
+let d = 2;
+
+console.log("++c =", ++c);
+console.log("c =", c);
+
+//Assignment operatiors
+let e = 5;
+let f = 2;
+
+e += 4; // e = e + 4
+console.log("e =", e);
